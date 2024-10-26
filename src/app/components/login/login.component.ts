9e2bd7461c2db84962ee4b42cbcd47a0fe1931d6
Lukas Huelshorst <l.huelshorst@babiel.com>
2024-10-26 13:48:49 +0200

    add login endpoints

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'kickathon-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+
+  public errors: any;
+  private _userService: UserService = inject(UserService);
+  private router: Router = inject(Router);
+  private fb: FormBuilder = inject(FormBuilder);
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(4)]],
+  });
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onLogin(): void {
+    this._userService.login(this.loginForm.value).then((token) => {
+      this._userService.setToken(token);
+      this.router.navigate(['leaderboard']);
+    }).catch((errors) => {
+      this.errors = errors;
+    });
+  }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,30 +1,24 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { JwtHelperService } from '@auth0/angular-jwt';
-import { map } from 'rxjs/operators';
+import axios from 'axios';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
 
-  constructor(
-    private http: HttpClient,
-    private router: Router,
-    private jwtHelper: JwtHelperService,
-  ) {}
+  private host: '/api';
 
-  public login(data: any): Observable < any > {
-    return this.http.post < any > (`${environment.endpoint}/v1/auth/login`, data)
-      .pipe(
-        map((auth: any) => {
-          if (auth && auth.token) {
-            this.setToken(auth.token);
-          }
-        })
-      );
+  private router: Router = inject(Router);
+  private jwtHelper: JwtHelperService = inject(JwtHelperService);
+
+  public login(data: any): Promise<any> {
+    return (await axios.post(`${this.host}/auth/login`, data)).data;
+  }
+
+  public register(data: any): Promise<any> {
+    return (await axios.post(`${this.host}/auth/register`, data)).data
   }
 
   public setToken(token: string): void {
@@ -40,15 +34,9 @@ export class UserService {
     this.router.navigate(['/login']);
   }
 
-  public register(data: any): Observable < any > {
-    return this.http.post < any > (`${environment.endpoint}/v1/auth/register`, data);
-  }
-
   public isAuthenticated(): boolean {
-    //console.log(this.getToken());
-
     return !this.jwtHelper.isTokenExpired(
-      this.getToken()
+      this.getToken(),
     );
   }
 
