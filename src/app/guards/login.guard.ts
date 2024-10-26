2be1d52c2fff434d4663bb8798f2a06b4d1b099f
Lukas Huelshorst <l.huelshorst@babiel.com>
2024-10-26 13:01:24 +0200

    create login and register component / Undo stupid change

---
+++ src/app/guards/login.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginGuard implements CanActivate {
+  constructor(private router: Router) {
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    if (true) {
+      return true;
+    }
+    this.router.navigate(['/login']);
+    return false;
+  }
+
+}
